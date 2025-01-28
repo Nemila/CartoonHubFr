e096e9bf96a4c71303a7712e0b012630dea51777
Lamine Diamoutene <pridila.2006@gmail.com>
2025-01-28 14:27:25 +0200

    .

--- src/app/api/update/route.ts
+++ src/app/api/update/route.ts
@@ -1,43 +1,43 @@
-// import MediaService from "@/features/media/server/db/media";
-// import prisma from "@/lib/prisma";
-// import { unstable_noStore } from "next/cache";
+import MediaService from "@/features/media/server/db/media";
+import prisma from "@/lib/prisma";
+import { unstable_noStore } from "next/cache";
 
-// export const GET = async () => {
-//   unstable_noStore();
-//   const perPage = 21;
-//   const totalMedia = 21 | 4571;
-//   const pages = Math.floor(totalMedia / perPage);
+export const GET = async () => {
+  unstable_noStore();
+  const perPage = 21;
+  const totalMedia = 21 | 4571;
+  const pages = Math.floor(totalMedia / perPage);
 
-//   const mediaService = new MediaService();
+  const mediaService = new MediaService();
 
-//   for (let i = 0; i < totalMedia; i++) {
-//     console.log(`Page ${i}/${pages}`);
+  for (let i = 0; i < totalMedia; i++) {
+    console.log(`Page ${i}/${pages}`);
 
-//     const medias = await prisma.media.findMany({
-//       orderBy: { popularity: "desc" },
-//       skip: i * perPage,
-//       take: perPage,
-//       select: {
-//         mediaType: true,
-//         tmdbId: true,
-//         season: true,
-//         title: true,
-//       },
-//     });
+    const medias = await prisma.media.findMany({
+      orderBy: { popularity: "desc" },
+      skip: i * perPage,
+      take: perPage,
+      select: {
+        mediaType: true,
+        tmdbId: true,
+        season: true,
+        title: true,
+      },
+    });
 
-//     for (const media of medias) {
-//       console.log("Updating media", media.title);
+    for (const media of medias) {
+      console.log("Updating media", media.title);
 
-//       const mediaCreateInput =
-//         media.mediaType === "series"
-//           ? await mediaService.getSerieCreateInput(media.tmdbId, media.season)
-//           : await mediaService.getMovieCreateInput(media.tmdbId);
+      const mediaCreateInput =
+        media.mediaType === "series"
+          ? await mediaService.getSerieCreateInput(media.tmdbId, media.season)
+          : await mediaService.getMovieCreateInput(media.tmdbId);
 
-//       const newMedia = await mediaService.upsert(mediaCreateInput);
+      const newMedia = await mediaService.upsert(mediaCreateInput);
 
-//       console.log("Media updated", newMedia.title);
-//     }
-//   }
+      console.log("Media updated", newMedia.title);
+    }
+  }
 
-//   return Response.json({ msg: "Hello World" });
-// };
+  return Response.json({ msg: "Hello World" });
+};
